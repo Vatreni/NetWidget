784fd42960a2c40ac99e52227289b35de2746013
Taras <gorthaur12@gmail.com>
2014-04-29 13:19:50 +0300

    init commit

---
+++ app/src/main/java/com/zaytsi/netwidget/app/Widget.java
@@ -0,0 +1,21 @@
+package com.zaytsi.netwidget.app;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Taras on 29.04.2014.
+ */
+public class Widget extends AppWidgetProvider {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
